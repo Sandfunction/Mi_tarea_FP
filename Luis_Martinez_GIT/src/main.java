5275d4280ab4af820a0dcb3b849c1fbdc429d100
Luis <Luis@DESKTOP-M5O248S>
2023-03-03 12:06:12 +0100

    añadida variable nombre

--- Luis_Martinez_GIT/src/main.java
+++ Luis_Martinez_GIT/src/main.java
@@ -4,6 +4,8 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Mi segundo Commit");
 		System.out.println("Mi primera branch");
+		
+		String nombre = "Luis";
 
 	}
 
